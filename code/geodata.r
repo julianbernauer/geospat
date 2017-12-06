e2d93625c5cfa36c9925abb9c057f2346c6903af
Julian Bernauer <julian.bernauer@googlemail.com>
2017-12-06 15:32:34 +0100

    Update geodata.r

--- code/geodata.r
+++ code/geodata.r
@@ -1,6 +1,7 @@
 #Getting geodata into R, writing geodata
 
 #Running example: AfD vote in Germany 
+#electoral districts: https://www.bundeswahlleiter.de/bundestagswahlen/2017/wahlkreiseinteilung/downloads.html
 
 #Google Maps 
 
